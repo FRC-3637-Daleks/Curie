aa39065256b56e8e50db9f920384ddd438cecafe
Grace Meiman <grace.meiman.142425@gmail.com>
2018-02-09 08:52:47 -0500

    Added the preliminary intake code classes

---
+++ Curie/src/Intake.h
@@ -0,0 +1,48 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Feb 6, 2018
+ *      Author: GEM
+ */
+
+#ifndef SRC_INTAKE_H_
+#define SRC_INTAKE_H_
+
+#include <Curie.h>
+
+#include <WPILib.h>
+#include <ctre/Phoenix.h>
+
+class Intake {
+  public:
+	typedef enum Motors MotorType_t;
+
+	Intake(int wristMotorChannel, int rollerMotorChannel, int intakeLowerLimit, int intakeUpperLimit, int intakeProximity);
+	Intake(WPI_TalonSRX* wristMotor, WPI_TalonSRX* rollerMotor, int intakeLowerLimit, int intakeUpperLimit, int intakeProximity);
+	Intake(WPI_TalonSRX& wristMotor, WPI_TalonSRX& rollerMotor, int intakeLowerLimit, int intakeUpperLimit, int intakeProximity);
+
+	void Raise();
+	void Lower();
+	void Push();
+	void Pull();
+	void StopWrist();
+	void StopRoller();
+	bool Proximity();
+	bool WristUpperLimit();
+	bool WristLowerLimit();
+
+	~Intake();
+
+  private:
+  	void InitDalekDrive();
+  	bool printFaults();
+  	WPI_TalonSRX *m_wristMotor;
+  	WPI_TalonSRX *m_rollerMotor;
+  	DigitalInput *m_lowerLimit;
+  	DigitalInput *m_upperLimit;
+	DigitalInput *m_proximity;
+  	bool m_needFree;
+
+};
+
+#endif /* SRC_INTAKE_H_ */
